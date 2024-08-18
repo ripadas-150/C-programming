3aa3e8e2d8e2e206947ab260e7d1db7e66288f58
Ripa Das <143581055+ripadas-150@users.noreply.github.com>
2024-08-18 22:36:20 +0600

    Add files via upload

---
+++ ascci charecter.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+     char c;
+     int d;
+     printf("Enter any ASCII charecter :");
+     scanf("%c ",&c);
+     printf("The ASCII value is :%c",c);
+     return 0;
+}
+
